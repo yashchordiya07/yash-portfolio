c5ab8d10b03a2a4eebb5c6c29d99df4d4f719e44
Yash Chordiya <yashchordiya7@gmail.com>
2026-04-14 12:10:57 +0530

    fix base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Aditya_Portfolio/', // This must match your repo name exactly
+  base: '/yash-portfolio/', // This must match your repo name exactly
 })
